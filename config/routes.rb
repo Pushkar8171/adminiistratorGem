a27c1fccd282919862f7660faf04afe1ebae19ec
Pushkar8171 <pushkarsinghrathore81@gmail.com>
2023-10-19 12:59:37 +0530

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :articles
+      resources :blogs
+      resources :todos
+
+      root to: "articles#index"
+    end
+  resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
