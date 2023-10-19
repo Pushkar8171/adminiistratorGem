aa4aed35cc8e213f1558f57ac2b3fd3082da2d69
Pushkar8171 <pushkarsinghrathore81@gmail.com>
2023-10-19 13:39:59 +0530

    Testing commits :(

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-      resources :articles
-      resources :blogs
-      resources :todos
+              resources :todos
 
       root to: "articles#index"
     end
